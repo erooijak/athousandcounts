b376b61a46a1bce23abab233c567cf4e3a5d75a4
unknown <erwinrooijakkers@gmail.com>
2014-05-03 19:46:30 +0200

    A thousand count display count

--- AThousandCounts/Scripts/scriptcam-settings.js
+++ AThousandCounts/Scripts/scriptcam-settings.js
@@ -34,7 +34,7 @@ function closeCamera() {
     $.scriptcam.closeCamera();
     setMessage();
 }
-function fileReady() {
+function fileReady(fileName) {
     $('#recorder').hide();
     $('#message').html('Thank you for participating at A Thousand Counts. Your clip will be processed into a video once we have received 1000 counts.<br/><br/>If you know other happy counters like you please invite them!');
     var fileNameNoExtension = fileName.replace(".mp4", "");
